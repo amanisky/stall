92386f2fbc279ebd622edce938871c2396b1b4eb
amanisky <zhangfand@isoftstone.com>
2018-12-07 17:36:50 +0800

    左右旋转

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,14 @@
       <el-collapse v-model="activeNames">
         <el-collapse-item title="通用" name="1">
           <div class="myicon">
-            <img @click="addToCanvas('deng')" src="@/images/deng.svg" alt="灯">
-            <img @click="addToCanvas('guizi')" src="@/images/guizi.svg" alt="柜子">
+            <img @click="addShape('deng')" src="@/images/deng.svg" alt="灯">
+            <img @click="addShape('guizi')" src="@/images/guizi.svg" alt="柜子">
           </div>
         </el-collapse-item>
         <el-collapse-item title="图标" name="2">
           <div class="myicon">
-            <img @click="addToCanvas('deng')" src="@/images/deng.svg" alt="灯">
-            <img @click="addToCanvas('guizi')" src="@/images/guizi.svg" alt="柜子">
+            <img @click="addShape('deng')" src="@/images/deng.svg" alt="灯">
+            <img @click="addShape('guizi')" src="@/images/guizi.svg" alt="柜子">
           </div>
         </el-collapse-item>
       </el-collapse>
@@ -20,21 +20,21 @@
     <el-container>
       <el-header>
         <el-button-group>
-          <el-button size="small" type="danger">删除选中</el-button>
-          <el-button size="small" type="danger">打通展位</el-button>
-          <el-button size="small" type="danger">恢复默认</el-button>
+          <el-button size="small" type="danger" @click="removeSelected">删除选中</el-button>
+          <el-button size="small" type="danger" @click="through">打通展位</el-button>
+          <el-button size="small" type="danger" @click="reset">恢复默认</el-button>
         </el-button-group>
         <el-button-group>
-          <el-button size="small" type="primary">左旋转</el-button>
+          <el-button size="small" type="primary" @click="leftRotate">左旋转</el-button>
           <el-button size="small" type="primary">右旋转</el-button>
           <el-button size="small" type="primary">调层距</el-button>
         </el-button-group>
         <el-button-group>
-          <el-button size="small">撤销</el-button>
-          <el-button size="small">重做</el-button>
+          <el-button size="small" type="info">撤销</el-button>
+          <el-button size="small" type="info">重做</el-button>
         </el-button-group>
         <el-button size="small" type="success">保存</el-button>
-        <el-button size="small" type="info">返回</el-button>
+        <el-button size="small">返回</el-button>
       </el-header>
       <el-main>
         <div id="canvas-box">
@@ -53,18 +53,22 @@ export default {
   data () {
     return {
       canvas: null,
-      activeNames: [1]
+      activeNames: [1],
+      canvasState: [],
+      canvasInitState: null
     }
   },
   mounted () {
-    fabric.Group.prototype.hasControls = false
+    fabric.Object.prototype.hasControls = false
     fabric.Group.prototype.lockMovementX = true
     fabric.Group.prototype.lockMovementY = true
+    fabric.Group.prototype.hoverCursor = 'default'
+    fabric.Group.prototype.selectable = false
     var canvasBox = document.getElementById('canvas-box')
     this.canvas = new fabric.Canvas(this.$refs.c, {
       width: canvasBox.clientWidth,
       height: canvasBox.clientHeight,
-      backgroundColor: '#ccc'
+      backgroundColor: '#fff'
     })
     fabric.Image.fromURL(this.requireSVG('stall'), (img) => {
       img.scale(2)
@@ -74,21 +78,52 @@ export default {
         img.clone(obj => {
           var text = new fabric.Text(`B${40 + i}`, { top: height })
           text.set('left', (width - text.width) / 2)
-          var group = new fabric.Group([obj, text], { left: width * i + 2, borderColor: 'red' })
+          var group = new fabric.Group([obj, text], { left: width * i + 2 })
           this.canvas.add(group)
+          this.canvasInitState = this.canvas.toJSON()
         })
       }
     })
   },
   methods: {
-    addToCanvas (url) {
+    // 添加形状
+    addShape (url) {
       fabric.Image.fromURL(this.requireSVG(url), (img) => {
         img.scale(0.3)
-        this.canvas.add(img)
+        this.canvas.add(img).bringToFront(img)
       })
     },
+    // 获取SVG
     requireSVG (name) {
       return require(`@/images/${name}.svg`)
+    },
+    // 删除选中
+    removeSelected () {
+      var activeObjects = this.canvas.getActiveObjects()
+      this.canvas.discardActiveObject()
+      if (activeObjects.length) {
+        this.canvas.remove.apply(this.canvas, activeObjects)
+      }
+    },
+    // 打通展位
+    through () {
+      var rect = new fabric.Rect({ top: 1.5, left: this.canvas.item(0).item(0).getScaledWidth() - 15, width: 30, height: this.canvas.item(0).item(0).getScaledHeight() - 4, fill: '#fff', lockMovementY: true })
+      this.canvas.add(rect).setActiveObject(rect)
+    },
+    // 恢复默认
+    reset () {
+      this.canvas.loadFromJSON(this.canvasInitState, this.canvas.renderAll.bind(this.canvas))
+    },
+    // 左旋转
+    leftRotate () {
+      var activeObject = this.canvas.getActiveObject()
+      console.log(activeObject.rotate)
+      // activeObject.set('rotate', activeObject.get('rotate') - 90).setCoords()
+      // this.canvas.requestRenderAll()
+    },
+    // 右旋转
+    rightRotate () {
+
     }
   }
 }
@@ -99,14 +134,14 @@ export default {
   height: 50px !important;
   color: #333;
   padding-top: 9px;
-  background-color: #B3C0D1;
+  background-color: #b3c0d1;
 }
 
 .draw .el-aside {
   height: 100vh;
   color: #333;
   text-align: center;
-  background-color: #D3DCE6;
+  background-color: #c3cce6;
   overflow-y: auto;
   overflow-x: hidden;
 }
@@ -114,7 +149,11 @@ export default {
 .draw .el-main {
   height: calc(100vh - 50px);
   color: #333;
-  background-color: #E9EEF3;
+  background-color: #e9eef3;
+}
+
+.draw .el-button-group {
+  margin-right: 10px;
 }
 
 #canvas-box {
