a86491d1e7ef8fa5d0bb2edb168e9d600c9ff4bc
amanisky <zhangfand@isoftstone.com>
2018-12-07 11:46:47 +0800

    新增操作

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,46 @@
 <template>
 <div id="app">
-  <el-container>
-    <el-header>装修展位</el-header>
+  <el-container class="draw">
+    <el-aside width="200px">
+      <el-collapse v-model="activeNames">
+        <el-collapse-item title="通用" name="1">
+          <div class="myicon">
+            <img @click="addToCanvas('deng')" src="@/images/deng.svg" alt="灯">
+            <img @click="addToCanvas('guizi')" src="@/images/guizi.svg" alt="柜子">
+          </div>
+        </el-collapse-item>
+        <el-collapse-item title="图标" name="2">
+          <div class="myicon">
+            <img @click="addToCanvas('deng')" src="@/images/deng.svg" alt="灯">
+            <img @click="addToCanvas('guizi')" src="@/images/guizi.svg" alt="柜子">
+          </div>
+        </el-collapse-item>
+      </el-collapse>
+    </el-aside>
     <el-container>
-      <el-aside width="300px">
-        <el-collapse v-model="activeNames">
-          <el-collapse-item title="通用" name="1">
-            <div class="myicon">
-              <img src="@/images/deng.svg" alt="灯">
-              <img src="@/images/guizi.svg" alt="柜子">
-            </div>
-          </el-collapse-item>
-          <el-collapse-item title="图标" name="2">
-            <div class="myicon">
-              <img src="@/images/deng.svg" alt="灯">
-              <img src="@/images/guizi.svg" alt="柜子">
-            </div>
-          </el-collapse-item>
-        </el-collapse>
-      </el-aside>
+      <el-header>
+        <el-button-group>
+          <el-button size="small" type="danger">删除选中</el-button>
+          <el-button size="small" type="danger">打通展位</el-button>
+          <el-button size="small" type="danger">恢复默认</el-button>
+        </el-button-group>
+        <el-button-group>
+          <el-button size="small" type="primary">左旋转</el-button>
+          <el-button size="small" type="primary">右旋转</el-button>
+          <el-button size="small" type="primary">调层距</el-button>
+        </el-button-group>
+        <el-button-group>
+          <el-button size="small">撤销</el-button>
+          <el-button size="small">重做</el-button>
+        </el-button-group>
+        <el-button size="small" type="success">保存</el-button>
+        <el-button size="small" type="info">返回</el-button>
+      </el-header>
       <el-main>
         <div id="canvas-box">
           <canvas ref="c"></canvas>
         </div>
       </el-main>
-      <el-aside width="200px">
-        111222
-      </el-aside>
     </el-container>
   </el-container>
 </div>
@@ -34,7 +48,6 @@
 
 <script>
 import { fabric } from 'fabric'
-import stall from '@/images/stall.svg'
 export default {
   name: 'App',
   data () {
@@ -53,32 +66,44 @@ export default {
       height: canvasBox.clientHeight,
       backgroundColor: '#ccc'
     })
-    fabric.loadSVGFromURL(stall, (objs, opts) => {
-      var obj = fabric.util.groupSVGElements(objs, opts)
-      for (var i = 0; i < 4; i++) {
-        obj.clone((clone) => {
-          clone.scale(2)
-          var text = new fabric.Text(`B${40 + i}`, { top: clone.get('height') * 2 })
-          text.set('left', (clone.get('width') * 2 - text.get('width')) / 2)
-          var group = new fabric.Group([clone, text], { left: clone.get('width') * 2 * i + 2, borderColor: 'red' })
+    fabric.Image.fromURL(this.requireSVG('stall'), (img) => {
+      img.scale(2)
+      var width = img.getScaledWidth()
+      var height = img.getScaledHeight()
+      for (let i = 0; i < 3; i++) {
+        img.clone(obj => {
+          var text = new fabric.Text(`B${40 + i}`, { top: height })
+          text.set('left', (width - text.width) / 2)
+          var group = new fabric.Group([obj, text], { left: width * i + 2, borderColor: 'red' })
           this.canvas.add(group)
         })
       }
     })
+  },
+  methods: {
+    addToCanvas (url) {
+      fabric.Image.fromURL(this.requireSVG(url), (img) => {
+        img.scale(0.3)
+        this.canvas.add(img)
+      })
+    },
+    requireSVG (name) {
+      return require(`@/images/${name}.svg`)
+    }
   }
 }
 </script>
 
-<style>
-.el-header, .el-footer {
-  background-color: #B3C0D1;
+<style scoped>
+.draw .el-header {
+  height: 50px !important;
   color: #333;
-  text-align: center;
-  line-height: 60px;
+  padding-top: 9px;
+  background-color: #B3C0D1;
 }
 
-.el-aside {
-  height: calc(100vh - 60px);
+.draw .el-aside {
+  height: 100vh;
   color: #333;
   text-align: center;
   background-color: #D3DCE6;
@@ -86,8 +111,8 @@ export default {
   overflow-x: hidden;
 }
 
-.el-main {
-  height: calc(100vh - 60px);
+.draw .el-main {
+  height: calc(100vh - 50px);
   color: #333;
   background-color: #E9EEF3;
 }
@@ -98,10 +123,13 @@ export default {
 
 .myicon {
   display: flex;
+  flex-wrap: wrap;
 }
 
 .myicon img {
   width: 30px;
   height: 30px;
+  margin-right: 10px;
+  cursor: pointer;
 }
 </style>
